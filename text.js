6b18a92a18b7787efed49ec804333bb8fc1e2c95
arieltordiglione <tordiglionea@gmail.com>
2023-04-06 18:19:11 -0300

    creacion NAV- arreglo parallax
    *Se modifico el NAV, ya que el anterior era solo para probar.
    *Arreglos esteticos en la animación SCROLL de JS

--- text.js
+++ text.js
@@ -1,3 +1,12 @@
+/* NAV RESPONSIVE */
+hamburger = document.querySelector('.hamburger');
+nav = document.querySelector('nav');
+hamburger.onclick = function () {
+	nav.classList.toggle('active');
+};
+
+/* PORTADA INDEX */
+let bg = document.getElementById('bg');
 let text = document.getElementById('text');
 let mountain1 = document.getElementById('mountain1');
 let mountain2 = document.getElementById('mountain2');
@@ -5,7 +14,10 @@ let mountain2 = document.getElementById('mountain2');
 window.addEventListener('scroll', () => {
 	let value = window.scrollY;
 
-	text.style.marginTop = value * 0.8 + 'px';
+	bg.style.width = 100 + value / 30 + '%';
+	text.style.marginTop = value * -0.7 + 'px';
 	mountain1.style.left = value * -2.5 + 'px';
+	mountain1.style.bottom = value * -1.5 + 'px';
 	mountain2.style.left = value * 2.5 + 'px';
+	mountain2.style.bottom = value * -1.5 + 'px';
 });
