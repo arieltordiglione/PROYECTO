2cf25ed007c907b3d296e67e86472da7687b626a
Ariel Tordiglione <tordiglionea@gmail.com>
2023-05-17 12:04:28 -0300

    incluimos API

--- script/api.js
+++ script/api.js
@@ -2,12 +2,13 @@ console.log('API.JS CARGADO CON EXITO');
 
 /* API REST */
 
-// Obtén una referencia al contenedor donde se mostrarán los hoteles
+/* // Obtén una referencia al contenedor donde se mostrarán los hoteles
 const hotelContainer = document.getElementById('hotel-container');
 
 // Realiza la solicitud a la API
 
-const url = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
+const url =
+	'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
 const options = {
 	method: 'GET',
 	headers: {
@@ -32,4 +33,48 @@ function displayData(data) {
 	dataContainer.innerHTML = JSON.stringify(data);
 }
 
-fetchData();
+fetchData(); */
+
+/* -------------------------------------- */
+
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
+const HTMLResponse = document.querySelector('#app');
+const ul = document.createElement('ul');
+
+fetch(`${API_URL}/users`)
+	.then((response) => response.json())
+	.then((users) => {
+		users.slice(0, 4).forEach((user, index) => {
+			let elem = document.createElement('li');
+			elem.appendChild(
+				document.createTextNode(`${user.name} - ${user.email}   `)
+			);
+
+			// Agrega texto diferente entre cada usuario
+			if (index === 0) {
+				elem.appendChild(document.createTextNode('  experiencia increíble'));
+			} else if (index === 1) {
+				elem.appendChild(
+					document.createTextNode(
+						' Cumplieron con todo lo estipulado, excelente la atención de todos'
+					)
+				);
+			} else if (index === 2) {
+				elem.appendChild(
+					document.createTextNode(
+						' Excelente servicio, sin dudas volvería a elegirlos.'
+					)
+				);
+			} else if (index === 3) {
+				elem.appendChild(
+					document.createTextNode(' Muy buena atención por Whatsapp')
+				);
+			}
+
+			ul.appendChild(elem);
+		}); /*  */
+		HTMLResponse.appendChild(ul);
+	});
+
+/* ★★★★★ - */
